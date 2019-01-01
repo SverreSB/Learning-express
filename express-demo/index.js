87003c3a73b0d347f9dfa099961c0a5e36009f68
Sverre <not.impotant@mail.com>
2018-12-31 19:16:48 -0800

    Added a route and logic  to show user based on id.

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const app = express();
 
-var users = ["David", "Oscar", "Alex", "Holly"];
+const users = [
+    {id: 1, name: "David"}, 
+    {id: 2, name: "Oscar"}, 
+    {id: 3, name: "Alex"}, 
+    {id: 4, name: "Holly"},
+];
 
 app.get('/', (req, res) =>{
     res.send('Hello World!!!!');
 });
 
+
 //when users branch is requested, the response is created and sent
 //This response is users in static array. 
 app.get('/api/users', (req, res) => {
-    var userString = "";
-    for(var user of users){
-        userString += user;
-        userString += "; ";
+    var userString = "[";
+    users.forEach(user => {
+        userString += `{id: ${user.id} name: ${user.name}}, `;
+        /*userString += user.name;
+        userString += "; ";*/
+    });
+    userString += "]";
+    res.send(`${userString}`); 
+});
+
+//Added logic to find user based on id in route. 
+app.get('/api/users/:id', (req, res) => {
+    const user = users.find(c => c.id === parseInt(req.params.id));
+    if(!user){
+        res.send("Error, user does not exist");
+    }else{
+        res.send(user);
     }
-    res.send(`${userString}`);
     
 });
 
