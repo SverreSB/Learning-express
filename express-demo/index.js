209c998256cd25921c6565a0bb5b46960d980ad6
Sverre <not.impotant@mail.com>
2019-01-01 12:39:44 -0800

    Added post route handler. Increments user array size by one to get id.

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const users = [
     {id: 1, name: "David"}, 
     {id: 2, name: "Oscar"}, 
@@ -26,6 +28,16 @@ app.get('/api/users', (req, res) => {
     res.send(`${userString}`); 
 });
 
+//When name is requested it creates a user with id array size + 1
+app.post('/api/users', (req, res) => {
+    const user = {
+        id: users.length + 1, 
+        name: req.body.name,
+    };
+    users.push(user);
+    res.send(user);
+});
+
 //Added logic to find user based on id in route. 
 app.get('/api/users/:id', (req, res) => {
     const user = users.find(c => c.id === parseInt(req.params.id));
@@ -34,7 +46,6 @@ app.get('/api/users/:id', (req, res) => {
     }else{
         res.send(user);
     }
-    
 });
 
 //Hard coded values are not good....
